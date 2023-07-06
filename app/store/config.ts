be511c7403d6b2b97394cfb1c3042ac38319d3f7
642024149 <642024149@qq.com>
2023-07-06 11:28:43 +0800

    更改样式

--- app/store/config.ts
+++ app/store/config.ts
@@ -22,7 +22,7 @@ export const DEFAULT_CONFIG = {
   fontSize: 14,
   theme: Theme.Auto as Theme,
   tightBorder: false,
-  sendPreviewBubble: true,
+  sendPreviewBubble: false,
   sidebarWidth: 300,
 
   disablePromptHint: false,
